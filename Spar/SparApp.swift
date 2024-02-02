e534d6d8a3d04078a1bc25b180cecd719880fded
Ksenia Kobak <79209115+Ksenia-Ksu@users.noreply.github.com>
2024-02-02 18:36:27 +0300

    Initial Commit

---
+++ Spar/SparApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparApp.swift
+//  Spar
+//
+//  Created by Ксения Кобак on 02.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
